87e83d08764edd04472957aa11d07946ead32f16
dawn012 <zhengyang040910@gmail.com>
2024-03-19 22:58:45 +0800

    create package for MVC

---
+++ src/main/java/com/mis/util/test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mis.util;
+
+/**
+ *
+ * @author zheng
+ */
+public class test {
+    
+}
